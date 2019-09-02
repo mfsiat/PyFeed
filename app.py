b40fc11ce2b661ba9bb5848b8a56844483691540
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-09-02 21:05:51 +0600

    added error message

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        # print(customer, dealer, rating, comments)
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='Please enter required fields')
+        return render_template('succes.html')
+
+
 if __name__== '__main__':
     app.debug = True
     app.run()
