03454e3693ee2afb87f304ca022e4f11364033aa
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-09-02 21:36:08 +0600

    configured database

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
+# Database setup
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/lexus'
+else: 
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
+# Routing 
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,7 +49,6 @@ def submit():
             return render_template('index.html', message='Please enter required fields')
         return render_template('succes.html')
 
-
+# Main function
 if __name__== '__main__':
-    app.debug = True
     app.run()
